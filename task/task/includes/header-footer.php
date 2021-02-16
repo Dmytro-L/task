06566818ae01af7cf97a171678cabaa9f75eca63
Dmytro-L <67592803+Dmytro-L@users.noreply.github.com>
2021-02-16 09:34:21 +0200

    Add files via upload

---
+++ task/task/includes/header-footer.php
@@ -0,0 +1,32 @@
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <div class="main-box clearfix">
+                <div class="action mt-3 mb-3">
+                    <button type="button" class="btn btn-success add mr-3 " data-toggle="modal"
+                        data-target="#exampleModalCenter" data-whatever="Add a new user">Add
+                    </button>
+                    <select class="all" name="all" size="1">
+                        <option value="0" selected>Please select</option>
+                        <option value="1">Set active</option>
+                        <option value="2">Set not active</option>
+                        <option value="3">Delete</option>
+                    </select>
+                    <div class="block_btn ml-3">
+                        <button type="button" name="btn_ok" class="btn_ok btn btn-success default" data-toggle="modal"
+                            data-target="#exampleModalCenter" data-whatever="Error">Ok</button>
+                        <button type="button" name="btn_ok" class="btn_ok btn btn-success all_active"
+                            data-toggle="modal" data-target="#exampleModalCenter"
+                            data-whatever="You are going to set the status to active for the selected users">Ok</button>
+                        <button type="button" name="btn_ok" class="btn_ok btn btn-success all_notactive"
+                            data-toggle="modal" data-target="#exampleModalCenter"
+                            data-whatever="You are going to set the status to not active for the selected users">Ok</button>
+                        <button type="button" name="btn_ok" class="btn_ok btn btn-success all_delete"
+                            data-toggle="modal" data-target="#exampleModalCenter"
+                            data-whatever="You are about to delete selected users">Ok</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
